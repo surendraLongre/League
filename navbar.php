261acabb5b64bdb38c83cc71d4f0ff847aa130b9
surendraLongre <murari.longre2366@gmail.com>
2023-07-03 23:36:00 +0000

    add navigation bar file

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<div id='container' class='margin5'>
+<nav>
+<div class="profile-pic">
+<img src="images/whatsapp_dp.jpg">
+</div>
+<ul>
+<li onclick="turn_red(1)">Home</li>
+<li onclick="turn_red(2)">Journey</li>
+<li onclick="turn_red(3)">Story</li>
+<li onclick="turn_red(4)">Inner Circle</li>
+</ul>
+</nav>
+</div>
