e4c183e988b46a5fed10d9b1576681ac29e8ed60
surendraLongre <murari.longre2366@gmail.com>
2023-07-03 23:36:28 +0000

    add story file

---
+++ story.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Journey</title>
+	<link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script> 
+</head>
+<body>
+	<?php
+		include 'navbar.php';
+	?>
+	<p class='text'></p>
+	<div class='margin5' style="white-space:pre-line;">
+		<?php
+			include 'imp_events.txt';
+		?>
+	</div>
+	<!-- start your code here -->
+</body>
+	<script src='javascript.js'></script>
+</html>
+
