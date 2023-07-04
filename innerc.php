f7a34f6d8ef40dcf5fd783f84ceb3c156efcdaba
surendraLongre <murari.longre2366@gmail.com>
2023-07-04 19:53:10 +0000

    add file for inner circle tab

---
+++ innerc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Journey</title>
+	<link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script> 
+</head>
+<body>
+	<?php
+		include 'navbar.php';
+	?>
+	<!-- start your code here -->
+	<p></p>
+	<div class='margin5' style="white-space:pre-line;">
+		<?php
+			include 'my_people.html';
+		?>
+	</div>
+
+</body>
+	<script src='javascript.js'></script>
+</html>
+
